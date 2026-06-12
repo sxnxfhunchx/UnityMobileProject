08fc8079b8d10c4a9530f9072eba7f6a6562e34c
Ekaterina Gorodetskaia <4414513+DyingDaylight@users.noreply.github.com>
2026-06-13 01:06:56 +0300

    EnergyBlastEffect

---
+++ Assets/Scripts/Ability/EnergyBlastAbility.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using SO.PowerUps;
+using UnityEngine;
+
+namespace Ability
+{
+    public class EnergyBlastAbility : PlayerAbility
+    {
+        private readonly EnergyBlastPowerUpData blastData;
+        private readonly Transform origin;
+
+        private const string BlastEffectTag = "BlastVFX";
+        
+        public EnergyBlastAbility(EnergyBlastPowerUpData data, MonoBehaviour coroutineRunner)
+            : base(data, coroutineRunner)
+        {
+            blastData = data;
+            origin = coroutineRunner.transform;
+        }
+
+        public override void Use()
+        {
+            if (!CanUse)
+                return;
+
+            Blast();
+            StartCooldown();
+        }
+
+        private void Blast()
+        {
+            ObjectPooler.Instance.SpawnFromPool(BlastEffectTag, origin.position, Quaternion.identity);
+            
+            Collider[] hits = Physics.OverlapSphere(origin.position, blastData.Radius);
+            
+            foreach (Collider hit in hits)
+            {
+                if (hit.TryGetComponent(out EnemyController enemyController))
+                {
+                    enemyController.TakeDamage(blastData.Damage);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SO/PowerUps/EnergyBlastPowerUpData.cs
@@ -0,0 +1,18 @@
+﻿using Ability;
+using Interfaces;
+using UnityEngine;
+
+namespace SO.PowerUps
+{
+    [CreateAssetMenu(menuName = "Power Ups/Energy Blast")]
+    public class EnergyBlastPowerUpData : PowerUpData
+    {
+        public float Radius = 6f;
+        public int Damage = 999;
+
+        public override IPlayerAbility CreateAbility(MonoBehaviour coroutineRunner)
+        {
+            return new EnergyBlastAbility(this, coroutineRunner);
+        }
+    }
+}
