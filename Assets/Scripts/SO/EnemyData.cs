a91d910e2266eadd9c6a7ef28590184cdcdb373a
tokyoboy701 <breakdance9696@gmail.com>
2026-06-18 15:31:34 +0300

    lvl diff
    lvl diff settings with SO

--- Assets/Scripts/SO/EnemyData.cs
+++ Assets/Scripts/SO/EnemyData.cs
@@ -5,8 +5,17 @@ public class EnemyData : ScriptableObject
 {
     public string enemyName;
     public string poolTag;
+    
+    [Header("Base Stats")]
     public int health = 1;
     public float speed = 15f;
-    public int scoreValue = 10;
     public int damage = 10;
+    public int scoreValue = 10;
+
+    [Header("Tier / Weapon Modifiers")]
+    public float rotationSpeedY = 180f; 
+    public float fireRateModifier = 1.0f; 
+    
+    [Header("Status")]
+    public bool isBerserkMode = false; 
 }
